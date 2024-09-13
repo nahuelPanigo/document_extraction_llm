1aed36c25bcecc69f3f00b570d31e272b1f8c2f7
nahuelpanigo <nahpanigo99@gmail.com>
2024-09-13 16:39:40 +0200

    new api fast requirement

--- fine_tunning/fine_tunning_per_validation.py
+++ fine_tunning/fine_tunning_per_validation.py
@@ -1,43 +1,25 @@
 import json
-#from transformers import LEDTokenizer, LEDForConditionalGeneration
-from transformers import AutoModelForCausalLM , AutoTokenizer,BitsAndBytesConfig
+from transformers import LEDTokenizer, LEDForConditionalGeneration
+#from transformers import AutoModelForCausalLM , AutoTokenizer,BitsAndBytesConfig
 from download_prepare_normalize_sedici_dataset.utils.read_and_write_files import read_data_json,detect_encoding
 from constant import JSONS_FOLDER,DATASET_WITH_TEXT_DOC,MAX_TOKENS_INPUT,MAX_TOKENS_OUTPUT,FINAL_MODEL_PATH,PROMPT
 import torch
+from generate_tokens import get_tokens
 
 filename_dataset = JSONS_FOLDER / DATASET_WITH_TEXT_DOC
 enc = detect_encoding(filename_dataset)["encoding"]
 dict_dataset =  read_data_json(filename_dataset,enc)
 
-
-
-
 data = {}
 total_len = len(dict_dataset)
-test_end = int(total_len * 0.9)
+test_end = int(total_len * 0.8)
 items_list = list(dict_dataset.values())
-data["validation"] = items_list[test_end:]
+data["test"] = items_list[test_end:int(total_len*0.9)]
 
-input_text = data["validation"][1]
 # Cargar el tokenizador y el modelo ajustado
 
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16
-)
-
-model = AutoModelForCausalLM.from_pretrained(
-    FINAL_MODEL_PATH,
-    quantization_config=bnb_config,  # Aplicar configuración de cuantización
-    low_cpu_mem_usage=True
-)
-
-tokenizer = AutoTokenizer.from_pretrained(FINAL_MODEL_PATH)
-
-#tokenizer = LEDTokenizer.from_pretrained(FINAL_MODEL_PATH)
-#model = LEDForConditionalGeneration.from_pretrained(FINAL_MODEL_PATH)
+tokenizer = LEDTokenizer.from_pretrained(FINAL_MODEL_PATH)
+model = LEDForConditionalGeneration.from_pretrained(FINAL_MODEL_PATH)
 
 # # Mover el modelo a la GPU si está disponible
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -47,10 +29,11 @@ model.to(device)
 model.eval()
 
 # # Ejemplo de texto de entrada
-input_to_check = data["validation"][3]
+input_to_check = data["test"][3]
 input_text = input_to_check["original_text"]
 
 input_to_check.pop("original_text")
+input_to_check.pop("dc.description.abstract")
 # Tokenizar la entrada
 inputs = tokenizer(f"{PROMPT}{input_text}", return_tensors="pt", truncation=True, padding=True,max_length=MAX_TOKENS_INPUT)
 
