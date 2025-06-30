97f0dc18ea92752410b391edd91dfb3573b5dc7b
nahuelPanigo <nahpanigo99@gmail.com>
2025-06-30 04:22:08 +0200

    add requirements badly excluded in gitignore

--- fine_tune_type/clasification.py
+++ fine_tune_type/clasification.py
@@ -1,8 +1,8 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from constants import JSON_FOLDER,DATASET_WITH_METADATA_AND_TEXT_DOC_CHECKED,DATASET_TYPE,PDF_FOLDER
-from utils.text_extraction.read_and_write_files import read_data_json,write_to_json
+from constants import JSON_FOLDER,DATASET_WITH_METADATA_AND_TEXT_DOC_CHECKED,DATASET_TYPE,PDF_FOLDER,DATA_FOLDER
+from utils.text_extraction.read_and_write_files import read_data_json,write_to_json,read_data_txt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from bs4 import BeautifulSoup
 import random
@@ -25,25 +25,38 @@ def clear_ids_no_pdf(data):
 
 
 
+TEXT_FOLDER = DATA_FOLDER /  "texts2"
+
+def filter_existing_ids(ids, folder):
+    return [id_ for id_ in ids if (folder / f"{id_}.txt").exists()]
 
 # Datos de entrada
 
 if __name__ == "__main__":
     load_dotenv()
-    filename = JSON_FOLDER / "textos_extraccion.json"
+    filename = JSON_FOLDER / DATASET_TYPE
     data = read_data_json(filename, "utf-8")
 
-    texts_tesis = data["tesis"][:1450]
-    texts_articulos= data["articulo"][:1450]
-    texts_libros = data["libro"][:1450]
+    id_tesis = data["Tesis"]
+    id_libros = data["Libro"]
+    id_articulos = data["Articulo"]
+
+    id_tesis = filter_existing_ids(id_tesis, TEXT_FOLDER)[:1450]
+    id_libros = filter_existing_ids(id_libros, TEXT_FOLDER)[:1450]
+    id_articulos = filter_existing_ids(id_articulos, TEXT_FOLDER)[:1450]
+
+    print(f"Se han encontrado {len(id_tesis)} tesis")
+    print(f"Se han encontrado {len(id_articulos)} articulos")   
+    print(f"Se han encontrado {len(id_libros)} libros")
+
+    texts_tesis = [read_data_txt(TEXT_FOLDER / f"{id_tesis[i]}.txt","utf-8") for i in range(len(id_tesis))]
+    texts_libros = [read_data_txt(TEXT_FOLDER / f"{id_libros[i]}.txt","utf-8") for i in range(len(id_libros))]
+    texts_articulos = [read_data_txt(TEXT_FOLDER / f"{id_articulos[i]}.txt","utf-8") for i in range(len(id_articulos))]
 
     token_extractor = os.getenv("EXTRACTOR_TOKEN")
 
     filename = JSON_FOLDER / "textos_extraccion.json"
-
-    print(f"Se han encontrado {len(texts_tesis)} tesis")
-    print(f"Se han encontrado {len(texts_articulos)} articulos")
-    print(f"Se han encontrado {len(texts_libros)} libros")
+    print("comenzando el entrenamiento")
 
 
     labels_tesis = ["tesis"] * len(texts_tesis)
@@ -54,7 +67,8 @@ if __name__ == "__main__":
     texts = texts_tesis + texts_articulos + texts_libros
     labels = labels_tesis + labels_articulos + labels_libros
 
-    texts = [clean_html(text) for text in texts]
+
+    #texts = [clean_html(text) for text in texts]
     # Shuffle
     import random
     combined = list(zip(texts, labels))
@@ -64,7 +78,7 @@ if __name__ == "__main__":
     # TF-IDF
     vectorizer = TfidfVectorizer(
         max_features=5000,
-        ngram_range=(1, 2),
+        ngram_range=(1, 4),
         analyzer='word',
         sublinear_tf=True,
     )
